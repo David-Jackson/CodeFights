938b0bce765498e8741a7d1107b388a23aff5e8b
David Jackson <jack8679@kettering.edu>
2017-05-09 14:05:40 -0400

    Added Arcade Intro Level 5

---
+++ Arcade/01 - Intro/Level 5/avoidObstacles/avoidObstacles.js
@@ -0,0 +1,24 @@
+﻿function avoidObstacles(inputArray) {
+    inputArray.sort(function (a, b) {
+        return a > b;
+    });
+    var jump = 1;
+    while (true) {
+        var success = true;
+        for (var i = 0; 
+             i <= inputArray[inputArray.length - 1]; 
+             i = i + jump) {
+            if (inputArray.indexOf(i) != -1) {
+                success = false;
+                break;
+            }
+        }
+        if (success) {
+            break;
+        } else {
+            jump++;
+        }
+    }
+    return jump;
+}
+​
---
+++ Arcade/01 - Intro/Level 5/isIPv4Address/isIPv4Address.js
@@ -0,0 +1,18 @@
+﻿function isIPv4Address(inputString) {
+    var res = false;
+    var arr = inputString.split(".");
+    if (arr.length != 4) return false;
+    for (var i = 0; i < arr.length; i++) {
+        console.log(arr[i], arr[i] >= 0, arr[i] <= 255)
+        if (arr[i] >= 0 && 
+            arr[i] <= 255 && 
+            arr[i] != "") {
+            res = true;
+        } else {
+            res = false;
+            break;
+        }
+    }
+    return res;
+}
+​
