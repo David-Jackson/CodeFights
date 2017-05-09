250c2f138726534e3601993a8d563f186cd3512b
David Jackson <jack8679@kettering.edu>
2017-05-09 06:50:42 -0400

    Added some from Arcade Core Well of Integration

---
+++ Arcade/Core/Well of Integration/01 - houseNumbersSum/houseNumbersSum.js
@@ -0,0 +1,6 @@
+﻿function houseNumbersSum(inputArray) {
+    var n = inputArray.shift();
+    if (n == 0) return 0;
+    return n + houseNumbersSum(inputArray);
+}
+​
---
+++ Arcade/Core/Well of Integration/02 - allLongestStrings/allLongestStrings.js
@@ -0,0 +1,15 @@
+﻿function allLongestStrings(inputArray) {
+    var max = arrayMaxLength(inputArray);
+    var res = []
+    for (var i = 0; i < inputArray.length; i++) {
+        if (inputArray[i].length == max) res.push(inputArray[i]);
+    }
+}
+​
+function arrayMaxLength(arr) {
+    var max = 0;
+    for (var i = 0; i < arr.length; i++) {
+        max = Math.max(max, arr[i].length);
+    }
+    return max;
+}
