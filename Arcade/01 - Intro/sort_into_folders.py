0772f061a5f91c2d94d8c08f165210136ee68e0e
David Jackson <jack8679@kettering.edu>
2017-05-09 14:07:23 -0400

    Added script to group same .js and .md files

---
+++ Arcade/01 - Intro/sort_into_folders.py
@@ -0,0 +1,13 @@
+import glob
+import os
+
+
+folder = "Level 7\\"
+
+
+for f in glob.glob(folder + "*"):
+    name = ".".join(os.path.basename(f).split(".")[:-1])
+    print name
+    if not os.path.exists(folder + name):
+        os.makedirs(folder + name)
+    os.rename(f, folder + name + "\\" + os.path.basename(f))
