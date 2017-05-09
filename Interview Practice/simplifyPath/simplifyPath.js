39dc2d8485c82c99294a085d51402cd4cf98b3db
David Jackson <jack8679@kettering.edu>
2017-05-09 13:25:13 -0400

    Added Interview Practice simplifyPath

---
+++ Interview Practice/simplifyPath/simplifyPath.js
@@ -0,0 +1,17 @@
+﻿function simplifyPath(path) {
+    var folders = path.split("/");
+    var simple = [];
+    for (var i = 0; i < folders.length; i++) {
+        var f = folders[i];
+        if (f == "..") {
+            simple.pop();
+        } else if (!(f == "" | f == ".")) {
+            simple.push(f);
+        }
+    }
+    simple = [""].concat(
+        (simple.length == 0) ? [""] : simple
+    );
+    return simple.join("/");
+}
+​
