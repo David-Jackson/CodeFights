daf0d4cdf1c6da0b5eacb2a79359b69926536ae6
David Jackson <jack8679@kettering.edu>
2017-05-11 15:46:10 -0400

    Added Arcade createAnagram

---
+++ Arcade/02 - Core/Mirror Lake/03 - Create Anagram/Create Anagram.js
@@ -0,0 +1,25 @@
+﻿function createAnagram(s, t) {
+    var sCount = charCount(s);
+    var tCount = charCount(t);
+    var diff = 0;
+    for (var key in sCount) {
+        diff += Math.abs(sCount[key] - tCount[key]);
+    }
+    return diff / 2;
+}
+​
+function charCount(str) {
+    var res = newAlphabetCharCodes();
+    for (var i = 0; i < str.length; i++) {
+        res[str.charCodeAt(i)] += 1;
+    }
+    return res;
+}
+         
+function newAlphabetCharCodes() {
+    var res = {};
+    for (var i = 65; i <= 90; i++) {
+        res[i] = 0;
+    }
+    return res;
+}
