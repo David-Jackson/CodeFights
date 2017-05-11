63a6008b20af8b69b5ab8de05d3b124e29cd6d7c
David Jackson <jack8679@kettering.edu>
2017-05-11 10:55:20 -0400

    Added Interview Practice nextLarger

---
+++ Interview Practice/nextLarger/nextLarger.js
@@ -0,0 +1,13 @@
+﻿function nextLarger(a) {
+    var res = [];
+    for (var i = 0; i < a.length; i++) {
+        res.push(-1);
+        for (var j = 0; j < res.length - 1; j++) {
+            if (res[j] == -1 && a[j] < a[i]) {
+                res[j] = a[i];
+            }
+        }
+    }
+    return res;
+}
+​
