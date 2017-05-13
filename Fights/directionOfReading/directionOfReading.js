5459fc91ed56feab76132ade253ea5a170ca60b0
David Jackson <jack8679@kettering.edu>
2017-05-13 11:10:35 -0400

    Added Fights folder

---
+++ Fights/directionOfReading/directionOfReading.js
@@ -0,0 +1,26 @@
+﻿function directionOfReading(numbers) {
+    var res = [];
+    for (var i = 0; i < numbers.length; i++) {
+        var arr = new Array(numbers.length);
+        arr.fill(0);
+        var pos = numbers.length - 1;
+        while (numbers[i] > 0) {
+            var r = numbers[i] % 10;
+            arr[pos] = r;
+            pos--;
+            numbers[i] -= r;
+            numbers[i] /= 10;
+        }
+        res.push(arr);
+    }
+    var output = new Array(numbers.length);
+    output.fill(0);
+    for (var x = 0; x < res[0].length; x++) {
+        for (var y = 0; y < res.length; y++) {
+            output[x] *= 10;
+            output[x] += res[y][x];
+        }
+    }
+    return output;
+}
+​
