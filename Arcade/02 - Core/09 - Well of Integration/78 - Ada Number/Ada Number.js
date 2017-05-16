12b0c5bc748bdee6bc767c96fe9eed1aa5b44c9f
David Jackson <jack8679@kettering.edu>
2017-05-16 10:17:58 -0400

    Finished Arcade Core Well of Integration

---
+++ Arcade/02 - Core/09 - Well of Integration/78 - Ada Number/Ada Number.js
@@ -0,0 +1,24 @@
+﻿function adaNumber(line) {
+    if (
+        line.charAt(0) == "_" || 
+        line.charAt(line.length - 1) == "_"
+    ) return false;
+    line = line.replace(/_/g, "");
+    var parts = line.split("#");
+    console.log(parts);
+    if (parts.length == 1) {
+        // needs to be base ten
+        return parts[0].match(/[0-9]/g).length == parts[0].length;
+    } else if (parts.length == 3) {
+        // can be different base
+        if (parts[0] == "" || parts[1] == "" | parts[2] != "") return false;
+        var base = parseInt(parts[0]);
+        if (base < 2 || base > 16) return false;
+        for (var i = 0; i < parts[1].length; i++) {
+            if (isNaN(parseInt(parts[1][i], base))) return false;
+        }
+        return true;
+    }
+    return false;
+}
+​
---
+++ Arcade/02 - Core/09 - Well of Integration/79 - Three Split/Three Split.js
@@ -0,0 +1,23 @@
+﻿function threeSplit(a) {
+    var count = 0;
+    var sum = a.reduce(function (a, b) {
+        return a + b;
+    }, 0);
+    var aim = sum / 3;
+    var ai = 0;
+    for (var i = 0; i < a.length - 2; i++) {
+        ai += a[i];
+        if (ai != aim) continue;
+        var aj = 0;
+        for (var j = i + 1; j < a.length - 1; j++) {
+            aj += a[j];
+            if (aj != aim) continue;
+            var ak = 0;
+            for (var k = j + 1; k < a.length; k++) {
+                ak += a[k];
+            }
+            if (ak == aj) count++;
+        }
+    }
+    return count;
+}
