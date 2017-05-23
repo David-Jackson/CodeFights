197e7eaeaaed7b0b811000a9df2b1b178bc1492d
David Jackson <jack8679@kettering.edu>
2017-05-23 12:54:10 -0400

    Added Challenge Solution

---
+++ Challenges/alienBit/alienBit.js
@@ -0,0 +1,9 @@
+﻿function alienBit(abit) {
+    var str = "";
+    for (var i = 2; i < abit.length; i = i + 3) {
+        var c = parseInt(abit.substr(i, 3));
+        str += String.fromCharCode(c);
+    }
+    return str;
+}
+​
