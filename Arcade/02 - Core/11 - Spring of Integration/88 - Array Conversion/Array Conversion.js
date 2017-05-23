096838851506cf24a3f2a2f66f9a5ea6371c70b3
David Jackson <jack8679@kettering.edu>
2017-05-23 15:55:26 -0400

    Added Arcade Solution

---
+++ Arcade/02 - Core/11 - Spring of Integration/88 - Array Conversion/Array Conversion.js
@@ -0,0 +1,17 @@
+﻿function arrayConversion(inputArray) {
+    var oddIter = true;
+    while (inputArray.length != 1) {
+        var arr = [];
+        for (var i = 0; i < inputArray.length; i = i + 2) {
+            if (oddIter) {
+                arr.push(inputArray[i] + inputArray[i + 1]);
+            } else {
+                arr.push(inputArray[i] * inputArray[i + 1]);
+            }
+        }
+        oddIter = !oddIter;
+        inputArray = arr;
+    }
+    return inputArray[0];
+}
+​
