f4a36542d9d1137e1ccd6775b1784b18c17cb2df
David Jackson <jack8679@kettering.edu>
2017-05-24 13:41:51 -0400

    Added Arcade Solution

---
+++ Arcade/02 - Core/11 - Spring of Integration/89 - Array Previous Less/Array Previous Less.js
@@ -0,0 +1,16 @@
+﻿function arrayPreviousLess(items) {
+    var res = [];
+    for (var i = 0; i < items.length; i++) {
+        var x = items[i];
+        var prev = -1;
+        for (var j = i - 1; j >= 0; j--) {
+            if (items[j] < items[i]) {
+                prev = items[j];
+                break;
+            }
+        }
+        res.push(prev);
+    }
+    return res;
+}
+​
