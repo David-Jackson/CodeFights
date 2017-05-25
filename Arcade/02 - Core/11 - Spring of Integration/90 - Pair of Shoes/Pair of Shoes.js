f9203e74d5228084289ea38bba891f5e1c551e47
David Jackson <jack8679@kettering.edu>
2017-05-25 15:51:57 -0400

    Added Arcade Solution

---
+++ Arcade/02 - Core/11 - Spring of Integration/90 - Pair of Shoes/Pair of Shoes.js
@@ -0,0 +1,16 @@
+﻿function pairOfShoes(shoes) {
+    while (shoes.length > 0) {
+        var shoe = shoes.pop();
+        var match = shoes.findIndex(function(element) {
+            return element[0] != shoe[0] && element[1] == shoe[1];
+        });
+        if (match == -1) {
+            shoes.push(shoe);
+            break;
+        } else {
+            shoes.splice(match, 1);
+        }
+    }
+    return shoes.length == 0;
+}
+​
