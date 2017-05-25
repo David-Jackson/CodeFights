eef6ea5db88e8beab0fe53b60b47d024d9b0be33
David Jackson <jack8679@kettering.edu>
2017-05-25 15:52:10 -0400

    Added Arcade Solution

---
+++ Arcade/02 - Core/11 - Spring of Integration/91 - Combs/Combs.js
@@ -0,0 +1,43 @@
+﻿function combs(comb1, comb2) {
+    function toBoolean(x) {
+        return (x == "*") ? true : false;
+    }
+    
+    function getValue(arr, index) {
+        // get value of arr if exists, else return false
+        if (
+            index < 0 || 
+            index >= index.length || 
+            arr.length == 0
+        ) return false;
+        return arr[index];
+    }    
+    var c1 = comb1.split("").map(toBoolean);
+    var c2 = comb2.split("").map(toBoolean);
+    
+    var padding = c1.length;
+    var minLength = c1.length + c2.length;
+    
+    for (var padding = c2.length; padding >= -c1.length; padding--) {
+        var valid = true;
+        for (var i = 0; i < c1.length; i++) {
+            if (c1[i] && getValue(c2, i + padding)) {
+                valid = false;
+                break;
+            }
+        }
+        if (valid) {
+            minLength = Math.min(
+                minLength,
+                Math.max(
+                    c1.length,
+                    c2.length,
+                    c1.length + padding,
+                    c2.length - padding
+                )
+            );
+        }
+    }
+    return minLength;
+}
+​
