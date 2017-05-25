02c278117640f67e2745f9a66e292a2d6a71fd39
David Jackson <jack8679@kettering.edu>
2017-05-25 15:52:33 -0400

    Added Arcade Solution

---
+++ Arcade/02 - Core/11 - Spring of Integration/93 - Cyclic String/Cyclic String.js
@@ -0,0 +1,15 @@
+﻿function cyclicString(s) {
+    var minCycleLen = s.length;
+    for (var i = 0; i < s.length - 1; i++) {
+        for (var j = i + 1; j <= s.length; j++) {
+            if (j - i > minCycleLen) continue;
+            var sub = s.substring(i, j);
+            var cycle = new Array(3 * s.length).fill(sub).join("");
+            if (cycle.indexOf(s) != -1) {
+                minCycleLen = Math.min(minCycleLen, sub.length);
+            }
+        }
+    }
+    return minCycleLen;
+}
+​
