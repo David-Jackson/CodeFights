ac328d33a243fa0bc62d9c53c7b47c747348048b
David Jackson <jack8679@kettering.edu>
2017-05-25 15:52:22 -0400

    Added Arcade Solution

---
+++ Arcade/02 - Core/11 - Spring of Integration/92 - Strings Crossover/Strings Crossover.js
@@ -0,0 +1,18 @@
+﻿function stringsCrossover(inputArray, result) {
+    function canCross(s1, s2, res) {
+        for (var i = 0; i < res.length; i++) {
+            if (s1[i] != res[i] && s2[i] != res[i]) return false;
+        }
+        return true;
+    }
+    var count = 0;
+    for (var i = 0; i < inputArray.length - 1; i++) {
+        var a = inputArray[i];
+        for (var j = i + 1; j < inputArray.length; j++) {
+            var b = inputArray[j];
+            if (canCross(a, b, result)) count++;
+        }
+    }
+    return count;
+}
+​
