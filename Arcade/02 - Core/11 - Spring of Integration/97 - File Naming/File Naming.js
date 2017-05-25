f97816a94652483f6cf33bcc45977fa1028e0221
David Jackson <jack8679@kettering.edu>
2017-05-25 15:53:24 -0400

    Added Arcade Solution #97

---
+++ Arcade/02 - Core/11 - Spring of Integration/97 - File Naming/File Naming.js
@@ -0,0 +1,19 @@
+﻿function fileNaming(names) {
+    for (var i in names) {
+        var name = names[i];
+        var index = names.indexOf(name);
+        if (index != i) {
+            // add file number to it
+            var fileNum = 0;
+            do {
+                fileNum++;
+                var newName = name + "(" + fileNum + ")";
+                index = names.indexOf(newName);
+                if (index == -1) break;
+            } while (index < i);
+            names[i] = newName;
+        }
+    }
+    return names;
+}
+​
