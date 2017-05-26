4deaac9a10e2c665c0704aafecf98d965ca309c7
David Jackson <jack8679@kettering.edu>
2017-05-26 09:35:29 -0400

    More Arcade Solutions

---
+++ Arcade/02 - Core/12 - List/100 - Reverse On Diagonals/Reverse On Diagonals.js
@@ -0,0 +1,12 @@
+﻿function reverseOnDiagonals(matrix) {
+    return matrix.map(function (itemY, y, mat) {
+        return itemY.map(function (itemX, x, arr) {
+            if (x == y || (arr.length - x - 1) == y) {
+                return mat[mat.length - 1 - y][arr.length - 1 - x];
+            } else {
+                return itemX;
+            }
+        });
+    });
+}
+​
---
+++ Arcade/02 - Core/12 - List/101 - Swap Diagonals/Swap Diagonals.js
@@ -0,0 +1,12 @@
+﻿function swapDiagonals(matrix) {
+    return matrix.map(function (itemY, y, mat) {
+       return itemY.map(function (itemX, x, arr) {
+           if (x == y || (arr.length - x - 1) == y) {
+               return mat[y][arr.length - 1 - x];
+           } else {
+               return itemX;
+           }
+       });
+    });
+}
+​
