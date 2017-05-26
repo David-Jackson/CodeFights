4a5bf17b78ec2588b382665c1c9860240e532f90
David Jackson <jack8679@kettering.edu>
2017-05-26 08:46:36 -0400

    Finished Arcade Spring of Integration

---
+++ Arcade/02 - Core/11 - Spring of Integration/94 - Beautiful Text/Beautiful Text.js
@@ -0,0 +1,15 @@
+﻿function beautifulText(inputString, l, r) {
+    for (var i = l; i <= r; i++) {
+        if ((inputString.length + 1) % (i + 1) == 0) {
+            valid = true;
+            for (var j = i; j < inputString.length; j = j + i + 1) {
+                if (inputString.charAt(j) != ' ') {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) return true;
+        }
+    }
+    return false;
+}
