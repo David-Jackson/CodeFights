d4cbd72c40bcb360efc24c4ee29aaeaa83928610
David Jackson <jack8679@kettering.edu>
2017-05-26 10:19:16 -0400

    Downloaded all images locally for .md files

---
+++ Repo Scripts/save_md_images.py
@@ -0,0 +1,24 @@
+import os
+import re
+import urllib.request
+
+
+def save_all_images_from_markdown(filename):
+    with open(filename) as f:
+        m = re.findall('!\[\]\(.+\)', f.read())
+        if m:
+            for res in m:
+                url = res[4:-1].split("?")[0]
+                directory = os.path.join(os.path.dirname(filename), "images")
+                name = os.path.join(directory, os.path.basename(url))
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
+                urllib.request.urlretrieve(url, name)
+                print(url)
+
+
+
+for root, dirs, files in os.walk(os.path.join("..", "Arcade")):
+    for file in files:
+        if file.endswith(".md"):
+             save_all_images_from_markdown(os.path.join(root, file))
