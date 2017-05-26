13a1d8633d4f90b297abd0e4e724577a2a383b8d
David Jackson <jack8679@kettering.edu>
2017-05-26 09:35:54 -0400

    Added Arcade Solution

---
+++ Arcade/02 - Core/12 - List/103 - Draw Rectangle/Draw Rectangle.js
@@ -0,0 +1,21 @@
+﻿function drawRectangle(canvas, rectangle) {
+    var x1 = rectangle[0],
+        y1 = rectangle[1],
+        x2 = rectangle[2],
+        y2 = rectangle[3];
+    return canvas.map(function (yArr, row, c) {
+        return yArr.map(function (xVal, col, arr) {
+            if (row == y1 || row == y2) {
+                if (col == x1 || col == x2) {
+                    xVal = "*";
+                } else if (col > x1 & col < x2) {
+                    xVal = "-";
+                }
+            } else if ((col == x1 || col == x2) && (row > y1 && row < y2)) {
+                xVal = "|";
+            }
+            return xVal;
+        });
+    });
+}
+​
