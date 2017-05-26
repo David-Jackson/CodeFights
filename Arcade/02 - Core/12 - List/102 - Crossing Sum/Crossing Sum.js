c26dc48fd92af598197057c62ccfd2e80ff820ac
David Jackson <jack8679@kettering.edu>
2017-05-26 09:35:40 -0400

    More Arcade Solutions

---
+++ Arcade/02 - Core/12 - List/102 - Crossing Sum/Crossing Sum.js
@@ -0,0 +1,13 @@
+﻿function crossingSum(matrix, a, b) {
+    function sum(acc, val) {
+        return acc + val;
+    }
+    return matrix.map(function(val, index, arr) {
+        if (index == a) {
+            return val.reduce(sum);
+        } else {
+            return val[b];
+        }
+    }).reduce(sum);
+}
+​
