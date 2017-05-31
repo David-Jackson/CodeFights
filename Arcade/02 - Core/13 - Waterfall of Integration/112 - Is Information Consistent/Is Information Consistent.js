3b4296c92cbf3755effad4893dce8cb84876788c
David Jackson <jack8679@kettering.edu>
2017-05-31 10:01:55 -0400

    Added Arcade Solution

---
+++ Arcade/02 - Core/13 - Waterfall of Integration/112 - Is Information Consistent/Is Information Consistent.js
@@ -0,0 +1,13 @@
+﻿function isInformationConsistent(evidences) {
+    for (var j = 0; j < evidences[0].length; j++) {
+        var guilty = false,
+            innocent = false;
+        for (var i = 0; i < evidences.length; i++) {
+            if (evidences[i][j] == 1) guilty = true;
+            if (evidences[i][j] == -1) innocent = true;
+        }
+        if (guilty && innocent) return false;
+    }
+    return true;
+}
+​
