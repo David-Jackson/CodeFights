14191c65de0b7807fd9210164b59f8d1ea52e824
David Jackson <jack8679@kettering.edu>
2017-06-05 11:46:03 -0400

    Added Arcade Solution

---
+++ Arcade/02 - Core/14 - Sorting Outpost/114 - Shuffled Array/Shuffled Array.js
@@ -0,0 +1,10 @@
+﻿function shuffledArray(shuffled) {
+    var arrSum = shuffled.reduce(function(a, b){
+        return a + b;
+    }, 0) / 2;
+    shuffled.splice(shuffled.indexOf(arrSum), 1);
+    return shuffled.sort(function(a, b) {
+        return a > b;
+    });
+}
+​
