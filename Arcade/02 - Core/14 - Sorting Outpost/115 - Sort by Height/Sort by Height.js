39c2849c1c1ec09de6f0bce6424d9f86bcbc96ec
David Jackson <jack8679@kettering.edu>
2017-06-06 14:24:24 -0400

    Added Arcade Solution

---
+++ Arcade/02 - Core/14 - Sorting Outpost/115 - Sort by Height/Sort by Height.js
@@ -0,0 +1,10 @@
+﻿function sortByHeight(a) {
+    var sorted = a.filter(n => n != -1).sort(function(a, b) {
+        return a < b;
+    });
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] != -1) a[i] = sorted.pop();
+    }
+    return a;
+}
+​
