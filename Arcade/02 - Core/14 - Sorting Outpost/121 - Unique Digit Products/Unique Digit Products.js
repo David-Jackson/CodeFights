07a2e51c6285ade3a3d3218116166d156ac2ce1c
David Jackson <jack8679@kettering.edu>
2017-06-08 10:25:16 -0400

    Added Arcade Solution

---
+++ Arcade/02 - Core/14 - Sorting Outpost/121 - Unique Digit Products/Unique Digit Products.js
@@ -0,0 +1,14 @@
+﻿function uniqueDigitProducts(a) {
+    return a.map(function(val) {
+        var p = 1;
+        while (val > 0) {
+            p *= (val % 10);
+            val = Math.floor(val / 10);
+        }
+        return p;
+    }).reduce(function(acc, val) {
+        if (acc.indexOf(val) == -1) acc.push(val);
+        return acc;
+    }, []).length;
+}
+​
