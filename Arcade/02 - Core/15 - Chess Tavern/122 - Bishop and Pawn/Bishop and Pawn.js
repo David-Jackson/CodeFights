870b502ea29087496ee84fe04f9cecdb2b0b9a60
David Jackson <jack8679@kettering.edu>
2017-06-09 08:44:52 -0400

    Added Arcade Solution

---
+++ Arcade/02 - Core/15 - Chess Tavern/122 - Bishop and Pawn/Bishop and Pawn.js
@@ -0,0 +1,8 @@
+﻿function bishopAndPawn(bishop, pawn) {
+    var by = bishop.charCodeAt(0),
+        bx = bishop.charCodeAt(1),
+        py = pawn.charCodeAt(0),
+        px = pawn.charCodeAt(1)
+​
+    return Math.abs((by - py) / (bx - px)) == 1;
+}
