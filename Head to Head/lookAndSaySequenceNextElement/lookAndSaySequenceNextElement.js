cb5d6e4128fcb23e694f662b86256e7027b28638
David Jackson <jack8679@kettering.edu>
2017-07-06 13:47:03 -0400

    Added H2H Solutions

---
+++ Head to Head/lookAndSaySequenceNextElement/lookAndSaySequenceNextElement.js
@@ -0,0 +1,16 @@
+﻿function lookAndSaySequenceNextElement(element) {
+    var res = "";
+    var count = 1;
+    var digit = element[0];
+    for (var i = 1; i < element.length; i++) {
+        if (digit == element[i]) {
+            count++;
+        } else {
+            res += count + digit;
+            digit = element[i];
+            count = 1;
+        }
+    }
+    return res + count + digit;
+}
+​
